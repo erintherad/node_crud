ce5c8f942311dcde0f5e168d9097fb09a91f2199
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-15 11:19:29 -0700

    adds mongodb to app with post route saving data

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+
+MongoClient.connect('mongodb://adidafhe10:eRad60165@node-list-app-shard-00-00-kk3sc.mongodb.net:27017,node-list-app-shard-00-01-kk3sc.mongodb.net:27017,node-list-app-shard-00-02-kk3sc.mongodb.net:27017/node-list-app?ssl=true&replicaSet=node-list-app-shard-0&authSource=admin', (err, database) => {
+  if(err) return console.log(err)
+  db = database
+  app.listen(3000, function() {
+    console.log('Listening on port 3000');
+  });
+})
 
 // Middleware that changes the request or response object before they get handled by the app.
 app.use(bodyParser.urlencoded({extended: true}))
@@ -10,9 +21,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/todos', (req, res) => {
-  console.log('hello');
+  db.collection('todos').save(req.body, (err, result) => {
+    if(err) return console.log(err)
+    
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
-
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
-});
