27b556229c1ed6a93277247a0a4e8e145d0e13d9
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-15 10:38:44 -0700

    Sets up read and create routes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.post('/todos', (req, res) => {
+  console.log('hello');
+})
+
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
